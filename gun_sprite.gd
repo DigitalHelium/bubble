49459f8f27cb80a7e5588bebd1eb6cff65f20bdc
tori-tih <98258927+tori-tih@users.noreply.github.com>
2025-06-12 14:34:18 +0400

    fix gun

--- gun_sprite.gd
+++ gun_sprite.gd
@@ -3,7 +3,7 @@ extends Sprite2D
 var radius := 32.0
 
 func _process(delta: float) -> void:
-	position = (get_global_mouse_position() - get_parent().position).normalized() * radius
-	var dir = Vector2(position - get_global_mouse_position());
-	print(dir.normalized())
-	look_at(get_global_mouse_position())
+	var direction = (get_global_mouse_position() - get_parent().global_position).normalized()
+	position = direction * radius
+	rotation = direction.angle()
+	#look_at(get_parent().global_position + direction)
