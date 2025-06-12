496d488220f1871766083ab7e4d482149bcc7e71
tori-tih <98258927+tori-tih@users.noreply.github.com>
2025-06-12 14:43:22 +0400

    fix direction gun fire

--- gun_sprite.gd
+++ gun_sprite.gd
@@ -6,4 +6,5 @@ func _process(delta: float) -> void:
 	var direction = (get_global_mouse_position() - get_parent().global_position).normalized()
 	position = direction * radius
 	rotation = direction.angle()
-	#look_at(get_parent().global_position + direction)
+	
+	flip_v = direction.x < 0
