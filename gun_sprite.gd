8d31b856b0eed1d403eff99c611b1d19cd8c9723
Mikhail Abramkin <amis.10@mail.ru>
2025-06-12 14:19:00 +0400

    вектор направления выстрела

--- gun_sprite.gd
+++ gun_sprite.gd
@@ -4,4 +4,6 @@ var radius := 32.0
 
 func _process(delta: float) -> void:
 	position = (get_global_mouse_position() - get_parent().position).normalized() * radius
+	var dir = Vector2(position - get_global_mouse_position());
+	print(dir.normalized())
 	look_at(get_global_mouse_position())
