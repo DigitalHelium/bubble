ba81a3444df9b353d3127d9403c334dc98a62ad0
tori-tih <98258927+tori-tih@users.noreply.github.com>
2025-06-14 03:20:24 +0400

    Плеер + настройки в игре и СМЕРТЬ

---
+++ test scenes/game_scene_test_with_tilemap.gd
@@ -0,0 +1,7 @@
+extends Node2D
+#var scene_setting = preload("res://setting/Control.tscn")
+#
+#func _on_setting_button_pressed() -> void:
+	#
+	#var scene =  scene_setting.instantiate()
+	#add_child(scene)
