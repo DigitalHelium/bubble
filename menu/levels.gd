fd2b7e6bfd8fc54d12c824bca01294ea05f1e133
tori-tih <98258927+tori-tih@users.noreply.github.com>
2025-06-14 08:19:17 +0400

    кат сцена 1

--- menu/levels.gd
+++ menu/levels.gd
@@ -1,26 +1,48 @@
 extends Node2D
 
+var flag = false
+var number = 1
+var one = load("res://texture/background_menu/cat-scene1.png")
+var two =  load("res://texture/background_menu/cat-scene2.png")
 
-func _on_menu_button_pressed() -> void:
-	$Music.volume_db = -100
-	SceneTransition.change_scene("res://menu/menu.tscn")
-
-
+func _process(delta: float) -> void:
+	if $next.is_hovered() and $"Sound button/SoundRate".time_left == 0 and !flag:
+		$"Sound button".play()
+		$"Sound button/SoundRate".start()
+		flag = true
+	if !$next.is_hovered() and flag:
+		$"Sound button/SoundRate".stop()
+		flag = false
+		
 func _on__pressed_1() -> void:
-	$Music.volume_db = -100
-	SceneTransition.change_scene("res://prod scenes/prod_scene.tscn")
-	DisplayServer.cursor_set_custom_image(load("res://texture/cursor-export.png"), 0, Vector2(0,0))
-	print('1')
+	$"Cat-scene0".set_texture("res://texture/background_menu/cat-scene1.png") 
 
 
-func _on__pressed_2() -> void:
-	$Music.volume_db = -100
-	SceneTransition.change_scene("res://Assets/Scenes/Levels/Level_2.tscn")
-	print('2')
-	pass # Replace with function body.
+func _on_next_pressed() -> void:
+	var texture = one
+	if number == 1:
+		texture = one
+		number = 2
+	elif number == 2:
+		texture = two
+		number = 0
+	else:
+		SceneTransition.change_scene("res://prod scenes/prod_scene.tscn")
+		DisplayServer.cursor_set_custom_image(load("res://texture/cursor-export.png"), 0, Vector2(0,0))
+		$AnimationPlayer.play("end")
+		return
+	
+	$AnimationPlayer.play("end")
+	await $AnimationPlayer.animation_finished
+	$"Cat-scene0".set_texture(texture) 
+	$AnimationPlayer.play("start")
+	
 
 
-func _on__pressed_3() -> void:
-	$Music.volume_db = -100
-	print('3')
-	pass # Replace with function body.
+func _on_next_mouse_entered() -> void:
+	$next.set_button_icon(load("res://menu/СтрелкаHover.png"))
+	
+
+
+func _on_next_mouse_exited() -> void:
+	$next.set_button_icon(load("res://menu/Стрелка0.png"))
